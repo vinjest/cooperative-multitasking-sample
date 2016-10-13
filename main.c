bc52cba8cecbe4d5e12d7a2eb3d22335baf90190
vinjest <renaissan7e@gmail.com>
2016-10-14 01:52:24 +0300

    third commit

--- main.c
+++ main.c
@@ -1,43 +1,56 @@
 #include "stdafx.h"
 #include "sheduler.h"
 #include "enums.h"
-
-#define separator -1
+#include "list.h"
 
 void func_first();
 void func_second();
 void func_third();
 
+struct thread_t* mutex;
+
 int main()
 {
-	exec(&func_first, &func_second, &func_third, separator, high, medium, low, separator);
+	puts("Main thread start!");
+	mutex = create_mutex();
+	struct thread_t* first = create_thread(&func_first, stack_allocate());
+	struct thread_t* second = create_thread(&func_second, stack_allocate());
+	struct thread_t* third = create_thread(&func_third, stack_allocate());
+	
+	join(first);
+	join(second);
+	join(third);
+	
+	puts("Main thread end!");
 	getchar();
 	return 0;
 }
 
 void func_first()
 {
-	sleep();
-	puts("First task start");
+	acquire_mutex(mutex);
+
+	puts("First thread start!");
 	next();
-	enter_critical_section();
-	puts("First task end!");
-	leave_critical_section();
+	puts("First thread end!");
+
+	release_mutex(mutex);
 }
 
 void func_second()
 {
-	enter_critical_section();
-	puts("Second task start!");
-	wakeup(&func_first);
+	acquire_mutex(mutex);
+
+	puts("Second thread start!");
 	next();
-	puts("Second task end!");
-	leave_critical_section();
+	puts("Second thread end!");
+
+	release_mutex(mutex);
 }
 
 void func_third()
 {
-	puts("Third task start!");
-	sleep();
-	puts("Third task end!");
+	puts("Third thread start!");
+	next();
+	puts("Third thread end!");
 }
