4474e104a2b23d19eeaae35582e645ea963ffec1
vinjest <renaissan7e@gmail.com>
2016-10-14 01:54:07 +0300

    third commit

--- sheduler.h
+++ sheduler.h
@@ -1,10 +1,19 @@
 #ifndef SHEDULER.H
 
-void exec(void(*p_func_first)(), ...);
-void next();
-void sleep();
-void wakeup(int* func_ptr);
-void enter_critical_section();
-void leave_critical_section();
+struct thread_t* create_thread(void(*func_ptr), void* stack_ptr);
+void* stack_allocate();
+void join(struct thread_t* thread_ptr);
+void set_priority(struct thread_t* thread_ptr, int priority);
+struct mutex_t* create_mutex();
+void acquire_mutex(struct mutex_t* mtx);
+void release_mutex(struct mutex_t* mtx);
+void resume(struct thread_t* thread_ptr);
+void pause(struct thread_t* thread_ptr);
+
+struct mutex_t
+{
+	int owner_id;
+	int counter;
+};
 
 #endif
