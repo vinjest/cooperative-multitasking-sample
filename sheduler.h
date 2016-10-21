5426e6f723f7d2d6d90edcd4a28fd51c5d019014
vinjest <renaissan7e@gmail.com>
2016-10-21 20:19:49 +0300

    Update sheduler.h

--- sheduler.h
+++ sheduler.h
@@ -1,19 +1,8 @@
-#ifndef SHEDULER.H
+#ifndef SHEDULER_H
+#define SHEDULER_H
 
-struct thread_t* create_thread(void(*func_ptr), void* stack_ptr);
-void* stack_allocate();
-void join(struct thread_t* thread_ptr);
-void set_priority(struct thread_t* thread_ptr, int priority);
-struct mutex_t* create_mutex();
-void acquire_mutex(struct mutex_t* mtx);
-void release_mutex(struct mutex_t* mtx);
-void resume(struct thread_t* thread_ptr);
-void pause(struct thread_t* thread_ptr);
-
-struct mutex_t
-{
-	int owner_id;
-	int counter;
-};
+void yield();
+void thread_start(void(*func_ptr)());
+void create_thread(void(*func_ptr)());
 
 #endif
