a9116efc9eb6837dcd2e1d7f38158dad08bffa6c
vinjest <renaissan7e@gmail.com>
2016-10-27 14:48:46 +0300

    Update sheduler.h

--- sheduler.h
+++ sheduler.h
@@ -2,8 +2,8 @@
 #define SHEDULER_H
 
 void yield();
-void thread_start(void(*func_ptr)());
+void thread_start(void(*func_ptr)(), struct thread_t* added_thread_ptr);
 void create_thread(void(*func_ptr)());
-void sleep(int milliseconds);
+void sleep(long milliseconds);
 
 #endif
