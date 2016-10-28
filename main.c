867e98eabce16c4f5d58a20db58f5c78f947bb3e
vinjest <renaissan7e@gmail.com>
2016-10-29 01:37:03 +0300

    Update main.c

--- main.c
+++ main.c
@@ -6,25 +6,33 @@ void func_second();
 
 int main()
 {
-    create_thread(&func_first);
-    create_thread(&func_second);
+    sleep(3000);
+    struct thread_t* first = create_thread(&func_first);
+    struct thread_t* second = create_thread(&func_second);
 
     yield();
 
+    join(first);
+    join(second);
+
+    sleep(3000);
+    puts("Main thread end!");
     return 0;
 }
 
 void func_first()
 {
+    sleep(5000);
     puts("First thread start!");
     yield();
     puts("First thread end!");
+
 }
 
 void func_second()
 {
+    sleep(10000);
     puts("Second thread start!");
     yield();
-    sleep(1000);
     puts("Second thread end!");
 }
