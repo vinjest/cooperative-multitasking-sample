5bd4e759b12a82cc55d32a4d83afb3d45ee57db2
vinjest <renaissan7e@gmail.com>
2016-10-29 01:38:33 +0300

    Update sheduler.h

--- sheduler.h
+++ sheduler.h
@@ -1,9 +1,21 @@
 #ifndef SHEDULER_H
 #define SHEDULER_H
 
+#include <setjmp.h>
+#include <stdbool.h>
+
 void yield();
-void thread_start(void(*func_ptr)(), struct thread_t* added_thread_ptr);
-void create_thread(void(*func_ptr)());
+struct thread_t* create_thread(void(*func_ptr)());
 void sleep(long milliseconds);
+void join(struct thread_t* thread);
+
+struct thread_t
+{
+    jmp_buf saved_context;
+    struct thread_t* next;
+    struct thread_t* joined_thread;
+    long wakeup_time;
+    bool isReady;
+};
 
 #endif
