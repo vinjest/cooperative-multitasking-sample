ad597f1186fa4b69d93f88c6ba0665b8bb925ede
vinjest <renaissan7e@gmail.com>
2016-10-29 01:38:56 +0300

    Update list.h

--- list.h
+++ list.h
@@ -8,13 +8,14 @@ struct thread_t
 {
     jmp_buf saved_context;
     struct thread_t* next;
-    bool isReady;
+    struct thread_t* joined_thread;
     long wakeup_time;
+    bool isReady;
 };
 
 struct thread_t* add_thread(struct thread_t** head);
 int remove_thread(struct thread_t** head, struct thread_t* thread);
-struct thread_t* get_next_thread(struct thread_t** head, struct thread_t* current_thread);
+struct thread_t* get_next_thread(struct thread_t** head, struct thread_t** current_thread);
 int clear(struct thread_t** head);
 int size(struct thread_t** head);
 
