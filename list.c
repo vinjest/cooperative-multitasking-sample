2d574326b98c46473ce56ff4637f142f803c12a9
vinjest <renaissan7e@gmail.com>
2016-11-03 23:59:24 +0300

    Update list.c

--- list.c
+++ list.c
@@ -1,224 +1,153 @@
-#include <malloc.h>
-#include <stdbool.h>
-#include <stdio.h>
 #include "list.h"
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
 
 struct thread_t* add_thread(struct thread_t** head);
-int remove_thread(struct thread_t** head, struct thread_t* thread);
-struct thread_t* get_next_thread(struct thread_t** head, struct thread_t** current_thread);
-struct thread_t* pop_front(struct thread_t** head);
-struct thread_t* pop_back(struct thread_t** head);
-int clear(struct thread_t** head);
+void remove_thread(struct thread_t** head, struct thread_t** removable);
+struct thread_t* next_thread(struct thread_t** head, struct thread_t** prev_thread);
+void clear(struct thread_t** head);
 int size(struct thread_t** head);
 
 struct thread_t* add_thread(struct thread_t** head)
 {
-    struct thread_t* current = *head;
-    struct thread_t* new_node = NULL;
-
-    new_node = (struct thread_t*)malloc(sizeof(struct thread_t));
-    if (!new_node)
-        return NULL;
-
-    new_node->next = NULL;
+    struct thread_t* new_node = (struct thread_t*)malloc(sizeof(struct thread_t));
+    new_node->joined_thread = NULL;
     new_node->isReady = true;
     new_node->wakeup_time = 0;
-    new_node->joined_thread = NULL;
 
     if (*head == NULL)
     {
         *head = new_node;
+        new_node->next = new_node;
+        new_node->prev = new_node;
     }
     else
     {
-        while (current->next != NULL)
-        {
+        struct thread_t* current = *head;
+
+        while (current->next != *head)
             current = current->next;
-        }
+
+        new_node->prev = current;
         current->next = new_node;
+
+        new_node->next = *head;
+        (*head)->prev = new_node;
     }
     return new_node;
 }
 
-int remove_thread(struct thread_t** head, struct thread_t* thread)
+struct thread_t* next_thread(struct thread_t** head, struct thread_t** prev_thread)
 {
-    struct thread_t* current = *head;
-
-    if (*head == NULL)
-        return -2;
-
-    if (*head == thread)
-    {
-        pop_front(head);
-        return 0;
-    }
-    else
-    {
-        while (current->next != NULL)
-        {
-            if (current->next == thread)
-            {
-                if ((current->next)->next == NULL)
-                {
-                    pop_back(head);
-                }
-                else
-                {
-                    current->next = (current->next)->next;
-                    (current->next)->next = NULL;
-                }
-                return 0;
-            }
-            current = current->next;
-        }
-    }
-    return -1;
-}
-
-struct thread_t* get_next_thread(struct thread_t** head, struct thread_t** previous_thread)
-{
-    struct thread_t* current = *head;
-    struct thread_t* previous = *previous_thread;
-
-    struct thread_t* first_among_the_sleepless = NULL;
-    struct thread_t* first_among_the_sleeping = NULL;
-
-    if (*head == NULL)
+    if (!*head)
         return NULL;
 
-    while (current->next != NULL)
+    struct thread_t* current = *head;
+
+    if (!((*prev_thread)->isReady))
+        current = *head;
+    else
+    {
+        while (current != *prev_thread)
+            current = current->next;
+        current = current->next;
+    }
+
+    for (;;)
     {
         if (current->isReady)
         {
             if (((clock() / CLOCKS_PER_SEC) * 1000) >= current->wakeup_time)
             {
-                if (current != previous)
+                if (current->joined_thread == NULL)
+                    return current;
+                else
                 {
-                    if (!first_among_the_sleepless)
-                        first_among_the_sleepless = current;
-                }
-            }
-            else
-            {
-                    if (!first_among_the_sleeping)
-                        first_among_the_sleeping = current;
-                    else
+                    if ((current->joined_thread)->isReady == false)
                     {
-                        if (current->wakeup_time < first_among_the_sleeping->wakeup_time)
-                            first_among_the_sleeping = current;
+                        current->joined_thread = NULL;
+                        return current;
                     }
-
+                }
             }
         }
         current = current->next;
     }
+}
 
-    if (current->isReady)
+void remove_thread(struct thread_t** head, struct thread_t** removable)
+{
+    if (!*head)
+        return;
+
+    if (*head == *removable)
     {
-        if (((clock() / CLOCKS_PER_SEC) * 1000) >= current->wakeup_time)
+        if ((*head)->next == *head)
         {
-            if (current != previous)
-            {
-                if (!first_among_the_sleepless)
-                    first_among_the_sleepless = current;
-            }
+            (*head)->next = NULL;
+            (*head)->prev = NULL;
+            *head = NULL;
         }
         else
         {
-                if (!first_among_the_sleeping)
-                    first_among_the_sleeping = current;
-                else
-                {
-                    if (current->wakeup_time < first_among_the_sleeping->wakeup_time)
-                        first_among_the_sleeping = current;
-                }
+            ((*head)->next)->prev = (*head)->prev;
+            ((*head)->prev)->next = (*head)->next;
+            struct thread_t* old_head = *head;
+            *head = (*head)->next;
+            old_head->next = NULL;
+            old_head->prev = NULL;
         }
     }
-
-    if (first_among_the_sleepless)
-        return first_among_the_sleepless;
     else
-        if (previous->isReady && (((clock() / CLOCKS_PER_SEC) * 1000) >= previous->wakeup_time))
-            return previous;
-    else
-        if (first_among_the_sleeping)
+    {
+        if (*removable != (*head)->prev)
         {
-            while(((clock() / CLOCKS_PER_SEC) * 1000) < first_among_the_sleeping->wakeup_time);
-            first_among_the_sleeping->wakeup_time = 0;
-            return first_among_the_sleeping;
+            ((*removable)->prev)->next = (*removable)->next;
+            ((*removable)->next)->prev = (*removable)->prev;
+            (*removable)->next = NULL;
+            (*removable)->prev = NULL;
         }
-
-    return NULL;
-}
-
-int clear(struct thread_t** head)
-{
-    struct thread_t* current = *head;
-    if (head == NULL)
-        return 1;
-
-    struct thread_t* temp = NULL;
-
-    while (current->next != NULL)
-    {
-        temp = current->next;
-        free(current);
-        current = temp;
+        else
+        {
+            ((*removable)->prev)->next = *head;
+            (*head)->prev = (*removable)->prev;
+            (*removable)->next = NULL;
+            (*removable)->prev = NULL;
+        }
     }
-    free(current);
-    *head = NULL;
-
-    return 0;
 }
 
-struct thread_t* pop_front(struct thread_t** head)
+void clear(struct thread_t** head)
 {
-    if (*head == NULL)
-        return NULL;
+    if (!*head)
+        return;
 
     struct thread_t* current = *head;
-    struct thread_t* result = (struct thread_t*)malloc(sizeof(struct thread_t));
-    result = current;
+    struct thread_t* removable = NULL;
 
-    *head = (*head)->next;
-
-    return result;
-}
-
-struct thread_t* pop_back(struct thread_t** head)
-{
-    if (*head == NULL)
-        return NULL;
-
-    struct thread_t* current = *head;
-    struct thread_t* temp = *head;
-    struct thread_t* result = (struct thread_t*)malloc(sizeof(struct thread_t));
-
-    while (current->next != NULL)
+    while (current->next != *head)
     {
-        temp = current;
+        removable = current;
         current = current->next;
+        free(removable);
     }
-    temp->next = NULL;
-    result = current;
-
-    return result;
+    *head = NULL;
 }
 
 int size(struct thread_t** head)
 {
-    if (*head == NULL)
-        return 0;
-
+    int count = 0;
     struct thread_t* current = *head;
 
-    int size = 0;
-
-    while (current->next != NULL)
+    if (*head)
     {
-        size++;
-        current = current->next;
+        while (current->next != *head)
+        {
+            count++;
+            current = current->next;
+        }
+        count++;
     }
-    size++;
-    return size;
+    return count;
 }
