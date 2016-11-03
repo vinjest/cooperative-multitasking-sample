7502be170efd7f0f523152548872e67d1146ddf3
vinjest <renaissan7e@gmail.com>
2016-11-03 23:57:27 +0300

    Update list.h

--- list.h
+++ list.h
@@ -3,20 +3,22 @@
 
 #include <setjmp.h>
 #include <time.h>
+#include <stdbool.h>
 
 struct thread_t
 {
     jmp_buf saved_context;
     struct thread_t* next;
+    struct thread_t* prev;
     struct thread_t* joined_thread;
     long wakeup_time;
     bool isReady;
 };
 
 struct thread_t* add_thread(struct thread_t** head);
-int remove_thread(struct thread_t** head, struct thread_t* thread);
-struct thread_t* get_next_thread(struct thread_t** head, struct thread_t** current_thread);
-int clear(struct thread_t** head);
+void remove_thread(struct thread_t** head, struct thread_t** removable);
+struct thread_t* next_thread(struct thread_t** head, struct thread_t** prev_thread);
+void clear(struct thread_t** head);
 int size(struct thread_t** head);
 
 #endif
