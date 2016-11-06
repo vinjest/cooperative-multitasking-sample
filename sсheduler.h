49968d3f1fc90074d8420e805f35a929eef84836
vinjest <renaissan7e@gmail.com>
2016-11-06 17:24:47 +0300

    Update sсheduler.h

--- sсheduler.h
+++ sсheduler.h
@@ -4,19 +4,20 @@
 #include <setjmp.h>
 #include <stdbool.h>
 
+struct mutex_t
+{
+    struct thread_t* owner;
+    struct waiting_thread_t* waiting_queue;
+    int count;
+};
+
 void yield();
+void acquire_mutex(struct mutex_t** mtx);
+void release_mutex(struct mutex_t** mtx);
+void mutex_init(struct mutex_t** mtx);
+void mutex_remove(struct mutex_t** mtx);
 struct thread_t* create_thread(void(*func_ptr)());
 void sleep(long milliseconds);
 void join(struct thread_t* thread);
-
-struct thread_t
-{
-    jmp_buf saved_context;
-    struct thread_t* next;
-    struct thread_t* prev;
-    struct thread_t* joined_thread;
-    long wakeup_time;
-    bool isReady;
-};
 
 #endif
