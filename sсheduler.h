64ee00f36c91b5ab931e44c711c1a40106236cd1
vinjest <renaissan7e@gmail.com>
2016-11-09 21:07:20 +0300

    Update sсheduler.h

--- sсheduler.h
+++ sсheduler.h
@@ -11,13 +11,30 @@ struct mutex_t
     int count;
 };
 
+struct sem_t
+{
+    struct waiting_thread_t* waiting_queue;
+    int waiting_count;
+    int max_value;
+    int value;
+};
+
+void* stack_allocate();
 void yield();
-void acquire_mutex(struct mutex_t** mtx);
-void release_mutex(struct mutex_t** mtx);
-void mutex_init(struct mutex_t** mtx);
-void mutex_remove(struct mutex_t** mtx);
 struct thread_t* create_thread(void(*func_ptr)());
+void thread_start(void(*func_ptr)(), struct thread_t* added_thread_ptr);
+void terminate_thread(struct thread_t* thread);
 void sleep(long milliseconds);
 void join(struct thread_t* thread);
+
+void mutex_init(struct mutex_t** mtx);
+void mutex_remove(struct mutex_t** mtx);
+void acquire_mutex(struct mutex_t** mtx);
+void release_mutex(struct mutex_t** mtx);
+
+void sem_init(struct sem_t** sem, int value);
+void sem_remove(struct sem_t** sem);
+void sem_up(struct sem_t** sem);
+void sem_down(struct sem_t** sem);
 
 #endif
